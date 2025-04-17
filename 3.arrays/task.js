b4a31d3f24aa2dd0fd00021d165472eeba18786b
pdmashkov <pdmashkov@rolftech.ru>
2025-04-17 13:07:58 +0300

    ДЗ 3, оба задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+    const result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+        acc += item;
+
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0);
+
+    return result;
 }
