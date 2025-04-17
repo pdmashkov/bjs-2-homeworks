25a161c78226944309f51d7695fcacc59f30a0ee
pdmashkov <pdmashkov@rolftech.ru>
2025-04-17 20:10:01 +0300

    ДЗ 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    let sum = 0;
+
+    if (this.marks && this.marks.length > 0) {
+        sum = this.marks.reduce((acc, val) => acc + val);
+        return sum / this.marks.length;
+    } else {
+        return sum;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
 }
