ffd819db0f1c821821b08ea7a4d4b4ddf08449d5
pdmashkov <pdmashkov@rolftech.ru>
2025-04-27 15:49:38 +0300

    ДЗ 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    return (...args) => {
+        const hash = md5(args);
+
+        let findHash = cache.find((item) => item.hash === hash);
+        if (findHash) {
+            return "Из кеша: " + findHash['value'];
+        }
+
+        let result = func(...args);
+        cache.push({
+            "hash": hash,
+            "value": result
+        });
+
+        if (cache.length > 5) {
+            cache.shift();
+        }
+
+        return "Вычисляем: " + result;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+    let isTrottled = false;
+
+    function wrapper(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func(args);
+            wrapper.count++;
+        }, delay);
+        if (!isTrottled) {
+            func(...args);
+            wrapper.count++;
+            isTrottled = true;
+        }
+        wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
